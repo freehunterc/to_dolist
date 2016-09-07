8651e8ec64c5386776172e1808bd63270007be9d
freehunterc <freehunterc@naver.com>
2016-09-07 22:50:08 +0900

    Initial Commit

---
+++ Todolist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todolist
+//
+//  Created by Eunkyo, Seo on 9/7/16.
+//  Copyright © 2016 Eunkyo, Seo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
